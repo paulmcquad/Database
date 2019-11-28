84366b36b4c6bdaa26f0a8d2353c56c5c1070bcc
Paul McQuade <paulmcquad@gmail.com>
2019-11-28 20:54:31 +0000

    Created a Dynamic Menu
    Created from 'smithside' database
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ PHP_and_MySQL/Lesson 30: Creating a Dynamic Menu/lesson30cs/index.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * index.php
+ *
+ * Main file
+ *
+ * @version    1.2 2011-02-03
+ * @package    Smithside Auctions
+ * @copyright  Copyright (c) 2011 Smithside Auctions
+ * @license    GNU General Public License
+ * @since      Since Release 1.0
+ */
+require_once 'includes/init.php';
+$logged_in = Contact::isLoggedIn();
+$accessLevel = Contact::accessLevel();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Smithside Auctions</title>
+<link href="css/main.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="container">
+
+  <div id="header">
+    <a href="index.php">
+      <img src="images/banner.jpg"  alt="Smithside Auctions" />
+    </a> 
+  </div><!-- end header -->
+  
+  <div id="navigation">
+    <h3 class="element-invisible">Menu</h3>
+    <ul class="mainnav">
+      <li><a href="index.php?content=categories">Lot Categories</a></li>
+      <li><a href="index.php?content=about">About Us</a></li>
+      <li><a href="index.php?content=home">Home</a></li>
+      <?php if ($logged_in) : ?>
+        <li><a href="index.php?content=logout">Logout</a></li>
+        <?php if ($accessLevel == 'Admin') : ?>
+          <li><a href="index.php?content=articles">Articles</a></li>
+        <?php endif; ?>
+      <?php else : ?>
+        <li><a href="index.php?content=login">Login</a></li>
+      <?php endif; ?>
+      <li><a href="index.php?content=articledisplay&id=1">Terms of Use</a></li>
+      <li><a href="index.php?content=articledisplay&id=2">Privacy Policy</a></li>
+
+    </ul>
+    <div class="clearfloat"></div>
+  </div><!-- end navigation -->
+
+  <div class="message">
+    <?php echo $message; ?> 
+  </div><!-- end message -->  
+  
+  <div class="sidebar">
+    <?php loadContent('sidebar', ''); ?>
+  </div><!-- end sidebar -->
+  
+  <div class="content">
+    <?php loadContent('content', 'home'); ?>
+  </div><!-- end content -->
+  
+  <div class="clearfloat"></div>
+  
+  <div id="footer">
+    <p>&copy; <?php echo date('Y'); ?>  Smithside Auctions</p>
+  </div><!-- end footer -->
+
+</div><!-- end container -->
+</body>
+</html>
